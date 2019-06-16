2b45ced6109ad25d14375a61efdd84ca0595cd88
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-15 20:57:22 -0400

    handlebars created!

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require("express");
-var exhbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var expressHandlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4200;
 var app = express();
 var router = express.Router();
 app.use(express.static(__dirname + "/public"));
 
-app.engine("handlebars", expressHandleBars({
+app.engine("handlebars", expressHandlebars({
     defaultLayout: "main"
 
 }));
@@ -20,8 +21,20 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb: //localhost/mongoHeadLines";
+
+mongoose.connect(db, function(error)    {
+
+    if(error)   {
+        console.log(error);
+    }
+else{
+    console.log("mongoose connection is working");
+}
+});
 app.listen(PORT, function() {
     console.log("listening on port" + PORT);
-}
+});
 
 
